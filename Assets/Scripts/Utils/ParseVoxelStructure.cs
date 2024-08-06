203bf7c3669383d3b506f72c1177c53ec2c072f8
sozooo <110661290+sozooo@users.noreply.github.com>
2024-08-06 18:09:07 +0300

    Parser + Models with Materials

---
+++ Assets/Scripts/Utils/ParseVoxelStructure.cs
@@ -0,0 +1,172 @@
+#if UNITY_EDITOR
+using System;
+using System.Collections.Generic;
+using System.IO;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+public class ParseVoxelStructure : MonoBehaviour
+{
+	struct RawVoxelData
+	{
+		private readonly Vector3 _position;
+		private readonly string _color;
+
+		public RawVoxelData(Vector3 position, string color)
+		{
+			_position = position;
+			_color = color;
+		}
+
+		public Vector3 Position => _position;
+		public string Color => _color;
+	}
+
+	[MenuItem("Tools/Parse Voxel Structure into Prefab")]
+	public static void Do()
+	{
+		if (Selection.assetGUIDs.Length == 0)
+			throw new InvalidOperationException("At least 1 text file should be selected!");
+
+		foreach(string guid in Selection.assetGUIDs)
+        {
+			var path = AssetDatabase.GUIDToAssetPath(guid);
+
+			StreamReader file = new(path);
+			var rawVoxelDatas = ParseRawVoxelsFromFile(file);
+			file.Close();
+
+			var rootGameObject = CreateVoxelStructure(rawVoxelDatas);
+			Selection.activeObject = rootGameObject;
+			PrefabUtility.SaveAsPrefabAsset(rootGameObject,
+				"Assets/Prefabs/VoxelStructures/" + Path.GetFileName(path) + ".prefab");
+			DestroyImmediate(rootGameObject);
+        }
+	}
+
+	private static List<RawVoxelData> ParseRawVoxelsFromFile(StreamReader file)
+	{
+		if (file == null)
+			throw new InvalidOperationException(nameof(file));
+
+		var rawVoxelDatas = new List<RawVoxelData>();
+		string line;
+
+		while ((line = file.ReadLine()) != null)
+		{
+			if (line[0] == '#' || line[0] == ' ')
+			{
+				continue;
+			}
+			else
+			{
+				var split = line.Split();
+
+                Vector3 position = new(float.Parse(split[0]),
+					float.Parse(split[2]), float.Parse(split[1]));
+
+                rawVoxelDatas.Add(new RawVoxelData(position, split[3]));
+			}
+		}
+
+		return rawVoxelDatas;
+	}
+
+	private static GameObject CreateVoxelStructure(List<RawVoxelData> rawVoxelData)
+	{
+		if (rawVoxelData == null)
+			throw new InvalidOperationException(nameof(rawVoxelData));
+
+		GameObject root = new();
+
+		foreach(RawVoxelData voxelData in rawVoxelData)
+        {
+			GameObject cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+
+			cube.transform.SetParent(root.transform);
+			cube.transform.position = voxelData.Position;
+
+			CreateTrailRenderer(cube);
+
+			Material material = GetOrCreateVoxelMaterial(voxelData.Color);
+			cube.GetComponent<Renderer>().sharedMaterial = material;
+		}
+
+		return root;
+	}
+
+	private static Material GetOrCreateVoxelMaterial(string colorCode)
+	{
+		Material material = AssetDatabase.LoadAssetAtPath(
+			$"Assets/Prefabs/VoxelStructures/Materials/{colorCode}.mat",
+			typeof(Material)) as Material;
+
+		if (material == null)
+			material = CreateVoxelMaterial(colorCode);
+
+		return material;
+	}
+
+	private static Material CreateVoxelMaterial(string colorCode)
+	{
+		var material = new Material(Shader.Find("FlexibleCelShader/Cel Silhouette"));
+        ColorUtility.TryParseHtmlString($"#{colorCode}", out Color color);
+
+        if (color == null)
+			throw new InvalidOperationException($"Couldn't parse color: #{colorCode}");
+
+		material.color = color;
+
+		AssetDatabase.CreateAsset(material, $"Assets/Prefabs/VoxelStructures/Materials/{colorCode}.mat");
+		return material;
+	}
+
+	private static void CreateTrailRenderer(GameObject voxel)
+    {
+		if (voxel == null)
+			throw new InvalidOperationException(nameof(voxel));
+
+		float startTime = 0;
+		float startValue = 1;
+		float endTime = 1;
+		float endValue = 0;
+
+		float trailTime = 0.14f;
+
+		string defaultColor = "ffffff";
+
+		TrailRenderer trail = voxel.AddComponent<TrailRenderer>();
+
+		AnimationCurve curve = new();
+		curve.AddKey(startTime, startValue);
+		curve.AddKey(endTime, endValue);
+
+		trail.widthCurve = curve;
+		trail.time = trailTime;
+
+		Gradient gradient = new();
+		gradient.alphaKeys = new GradientAlphaKey[] {
+			new GradientAlphaKey(startValue, startTime),
+			new GradientAlphaKey(endValue, endTime) };
+
+		trail.colorGradient = gradient;
+
+		trail.shadowCastingMode = ShadowCastingMode.Off;
+		trail.receiveShadows = false;
+
+		Material material = CreateVoxelMaterial(defaultColor);
+
+		if (material == null)
+			throw new ArgumentNullException(nameof(material));
+
+		trail.SetSharedMaterials(new List<Material>() { material });
+
+		//trail.SetMaterials(new List<Material>() { material });
+
+		//trail.materials = new Material[] { material };
+
+		trail.enabled = false;
+    }
+}
+#endif
