0966f2f34e770bb8bfa4bc6f6e3f1505bc9c8336
sozooo <110661290+sozooo@users.noreply.github.com>
2024-11-08 17:36:06 +0300

    Yandex Game SDK

---
+++ Assets/Scripts/YandexGame/WorkingData/SavesYG.cs
@@ -0,0 +1,24 @@
+﻿
+namespace YG
+{
+    [System.Serializable]
+    public class SavesYG
+    {
+        public int idSave;
+        public bool isFirstSession = true;
+        public string language = "ru";
+        public bool promptDone;
+        
+        public float MusicLevel = 1.0f;
+        public float SoundLevel = 1.0f;
+        public bool Muted = false;
+
+        public int Coins = 0;
+        public float Highscore = 0;
+
+        public int TimerLevel = 0;
+        public int BlastRadiusLevel = 0;
+
+        public int CoinsForAd = 500;
+    }
+}
