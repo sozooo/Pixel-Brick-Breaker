43093b89b4bde7c3997f45669adb2114528d76d7
sozooo <110661290+sozooo@users.noreply.github.com>
2025-06-02 12:17:02 +0300

    Bullet Despawn Timer

--- Assets/Project/Scripts/Bullet/Bullet.cs
+++ Assets/Project/Scripts/Bullet/Bullet.cs
@@ -1,9 +1,14 @@
 using System;
+using System.Threading;
+using Cysharp.Threading.Tasks;
 using UnityEngine;
 
 public class Bullet : MonoBehaviour, ISpawnable<Bullet>
 {
+    [SerializeField] private float _lifeTime = 6f;
+    
     private Transform _transform;
+    private CancellationTokenSource _cancellationToken;
 
     public event Action<Bullet> Despawned;
 
@@ -15,11 +20,25 @@ public class Bullet : MonoBehaviour, ISpawnable<Bullet>
     private void OnDisable()
     {
         Despawned?.Invoke(this);
+        
+        _cancellationToken?.Cancel();
     }
 
     public void Initialize(Vector3 position, Quaternion rotation)
     {
         _transform.position = position;
         _transform.rotation = rotation;
+        
+        _cancellationToken?.Cancel();
+        _cancellationToken = new CancellationTokenSource();
+        
+        TimerBeforeDespawn(_cancellationToken.Token).Forget();
+    }
+
+    private async UniTaskVoid TimerBeforeDespawn(CancellationToken token)
+    {
+        await UniTask.WaitForSeconds(_lifeTime, cancellationToken: token);
+        
+        Despawned?.Invoke(this);
     }
 }
