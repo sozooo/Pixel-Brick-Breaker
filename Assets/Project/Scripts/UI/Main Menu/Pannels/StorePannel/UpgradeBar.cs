06e3fcb2c60568b45a535e9070f29139a5b0224e
Marlen Epifiev <svetlana.khalilova.83@mail.ru>
2026-03-18 22:26:47 +0300

    Update UI Main Menu

--- Assets/Project/Scripts/UI/Main Menu/Pannels/StorePannel/UpgradeBar.cs
+++ Assets/Project/Scripts/UI/Main Menu/Pannels/StorePannel/UpgradeBar.cs
@@ -1,3 +1,4 @@
+using DG.Tweening;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -5,16 +6,25 @@ namespace Project.Scripts.UI.Main_Menu.Pannels.StorePannel
 {
     public class UpgradeBar : MonoBehaviour
     {
-        private const float FillPerLevel = 0.33f;
+        private const int MaxLevel = 3;
 
         [SerializeField] private UpgradeItem _upgradeItem;
-        [SerializeField] private Image _fill;
+        [SerializeField] private Slider _slider;
+        
+        private Tween _tween;
+
+        private void Awake()
+        {
+            _slider.minValue = 0;
+            _slider.maxValue = MaxLevel;
+            _slider.wholeNumbers = true;
+        }
 
         private void OnEnable()
         {
             _upgradeItem.Upgraded += Fill;
         }
-    
+
         private void OnDisable()
         {
             _upgradeItem.Upgraded -= Fill;
@@ -22,7 +32,10 @@ namespace Project.Scripts.UI.Main_Menu.Pannels.StorePannel
 
         private void Fill(int level)
         {
-            _fill.fillAmount = FillPerLevel * level;
+            float target = level;
+
+            _tween?.Kill();
+            _tween = DOTween.To(() => _slider.value, x => _slider.value = x, target, 0.25f);
         }
     }
 }
