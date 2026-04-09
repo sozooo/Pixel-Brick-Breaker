e7edfc11e275f087fbb5734f308b619da78a42cb
Marlen Epifiev <svetlana.khalilova.83@mail.ru>
2026-04-09 12:18:58 +0300

    Make build

---
+++ Assets/Project/Scripts/WorkObjects/Handlers/AdvPauseAndSoundWatcher.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using YG;
+
+namespace Project.Scripts.WorkObjects.Handlers
+{
+    public class AdvPauseAndSoundWatcher : MonoBehaviour
+    {
+        private void OnEnable()
+        {
+            YG2.onOpenAnyAdv += Mute;
+            YG2.onCloseAnyAdv += Unmute;
+        }
+
+        private void OnDisable()
+        {
+            YG2.onOpenAnyAdv -= Mute;
+            YG2.onCloseAnyAdv -= Unmute;
+        }
+
+        private void Mute()
+        {
+            Time.timeScale = 0f;
+            AudioListener.pause = true;
+        }
+
+        private void Unmute()
+        {
+            Time.timeScale = 1f;
+            AudioListener.pause = false;
+        }
+    }
+}
---
+++ Assets/Project/Scripts/WorkObjects/Handlers/GRA.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using YG;
+
+namespace Project.Scripts.WorkObjects.Handlers
+{
+    public class GRA : MonoBehaviour
+    {
+        private void Start() =>
+            YG2.GameReadyAPI();
+    }
+}
